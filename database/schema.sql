491f1ec516af45e2fc9d82eca39a872a47d8dc87
Rieze Andrei Venzon <riezeandreivenzon@gmail.com>
2025-10-02 22:15:38 +0800

    Added email table for users

--- database/schema.sql
+++ database/schema.sql
@@ -8,6 +8,7 @@ USE fit_and_brawl_gym;
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     role ENUM('member', 'admin', 'trainer') DEFAULT 'member'
 );
