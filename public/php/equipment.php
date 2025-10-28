8b81b6bc7fe9107e629a4ec06b621a076493b7b0
kell <eabondoc2024@plm.edu.ph>
2025-10-28 13:19:09 +0800

    Created a Separate Header and Footer PHP Files for Better Consistency and Integration

--- public/php/equipment.php
+++ public/php/equipment.php
@@ -280,44 +280,6 @@ if ($hasActiveMembership) {
 
     </main>
 
-    <!--Footer-->
-    <footer>
-        <div class="container footer-flex">
-            <div class="footer-logo-block">
-                <img src="../../images/footer-title.png" alt="FITXBRAWL" class="footer-logo-title">
-            </div>
-            <div class="footer-menu-block">
-                <div class="footer-menu-title">MENU</div>
-                <ul class="footer-menu-list">
-                    <li><a href="index.php">Home</a></li>
-                    <li><a href="membership.php">Membership</a></li>
-                    <li><a href="equipment.php">Equipment</a></li>
-                    <li><a href="products.php">Products</a></li>
-                    <li><a href="contact.php">Contact</a></li>
-                    <li><a href="feedback.php">Feedback</a></li>
-                </ul>
-            </div>
-            <div class="footer-contact-block">
-                <div class="footer-contact-title">CONTACT</div>
-                <div class="footer-contact-details">
-                    1832 Oroquieta Rd, Santa Cruz, Manila,<br>
-                    1008 Metro Manila<br><br>
-                    Gmail: fitxbrawl@gmail.com
-                </div>
-            </div>
-            <div class="footer-hours-block">
-                <div class="footer-hours-title">OPENING HOURS</div>
-                <div class="footer-hours-details">
-                    Sun–Fri: 9AM to 10PM<br>
-                    Saturday: 10AM to 7PM
-                </div>
-            </div>
-        </div>
-        <div class="copyright">
-            <p>&copy; 2025 Fit X Brawl, All rights reserved.</p>
-        </div>
-    </footer>
-
     <script>
         // Load equipment data
         fetch('equipment.php?api=true')
@@ -334,5 +296,5 @@ if ($hasActiveMembership) {
             .catch(error => console.error('Error loading equipment:', error));
     </script>
     <script src="../js/equipment.js"></script>
-</body>
-</html>
+
+<?php require_once '../../includes/footer.php'; ?>
--- public/php/feedback.php
+++ public/php/feedback.php
@@ -159,78 +159,15 @@ if (isset($_SESSION['email']) && isset($_SESSION['avatar'])) {
     $hasCustomAvatar = $_SESSION['avatar'] !== 'default-avatar.png' && !empty($_SESSION['avatar']);
     $avatarSrc = $hasCustomAvatar ? "../../uploads/avatars/" . htmlspecialchars($_SESSION['avatar']) : "../../images/profile-icon.svg";
 }
+
+// Set variables for header
+$pageTitle = "Fit and Brawl - Feedback";
+$currentPage = "feedback";
+$additionalCSS = ["../css/pages/feedback.css?=v2"];
+
+// Include header
+require_once '../../includes/header.php';
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fit and Brawl</title>
-    <link rel="stylesheet" href="../css/global.css">
-    <link rel="stylesheet" href="../css/pages/feedback.css?=v2">
-    <link rel="stylesheet" href="../css/components/footer.css">
-    <link rel="stylesheet" href="../css/components/header.css">
-    <link rel="shortcut icon" href="../../images/fnb-icon.png" type="image/x-icon">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet">
-    <script src="https://kit.fontawesome.com/7d9cda96f6.js" crossorigin="anonymous"></script>
-    <script src="../js/header-dropdown.js"></script>
-    <script src="../js/hamburger-menu.js"></script>
-    <?php if (SessionManager::isLoggedIn()): ?>
-        <link rel="stylesheet" href="../css/components/session-warning.css">
-        <script src="../js/session-timeout.js"></script>
-    <?php endif; ?>
-</head>
-
-<body>
-    <!--Header-->
-    <header>
-        <div class="wrapper">
-            <button class="hamburger-menu" aria-label="Toggle menu">
-                <span></span>
-                <span></span>
-                <span></span>
-            </button>
-            <div class="title">
-                <a href="index.php">
-                    <img src="../../images/fnb-logo-yellow.svg" alt="Logo" class="fnb-logo">
-                </a>
-                <a href="index.php">
-                    <img src="../../images/header-title.svg" alt="FITXBRAWL" class="logo-title">
-                </a>
-            </div>
-            <nav class="nav-bar">
-                <ul>
-                    <li><a href="index.php">Home</a></li>
-                    <li><a href="<?= $membershipLink ?>">Membership</a></li>
-                    <li><a href="equipment.php">Equipment</a></li>
-                    <li><a href="products.php">Products</a></li>
-                    <li><a href="contact.php">Contact</a></li>
-                    <li><a href="feedback.php" class="active">Feedback</a></li>
-                </ul>
-            </nav>
-            <?php if (isset($_SESSION['email'])): ?>
-                <!-- Logged-in dropdown -->
-                <div class="account-dropdown">
-                    <img src="<?= $avatarSrc ?>" alt="Account" class="account-icon">
-                    <div class="dropdown-menu">
-                        <a href="user_profile.php">Profile</a>
-                        <a href="logout.php">Logout</a>
-                    </div>
-                </div>
-            <?php else: ?>
-                <!-- Not logged-in -->
-                <a href="login.php" class="account-link">
-                    <img src="../../images/profile-icon.svg" alt="Account" class="account-icon">
-                </a>
-            <?php endif; ?>
-        </div>
-    </header>
 
     <!--Main-->
     <main>
